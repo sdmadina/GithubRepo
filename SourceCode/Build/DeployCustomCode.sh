591809d2ba74c486f8fdfc8be445d1ac284b492e
Devendra Bobde <devendra.bobde@itcinfotech.com>
2016-09-14 15:15:46 +0530

    Modified script for testing ant command

--- SourceCode/Build/DeployCustomCode.sh
+++ SourceCode/Build/DeployCustomCode.sh
@@ -2,7 +2,6 @@
 ### Steps to deploy custom code ###
 
 ### 1. Copy the repository to windchill folder ###
-sudo su -
 echo "Start : Copying the repository to windchill folder"
 cp -rf /home/GithubRepo/SourceCode /opt/ptc/Windchill_11.0/Windchill/
 echo "End : Copying the repository to windchill folder"
@@ -17,16 +16,19 @@ echo "End : Navigating to windchill shell"
 ### 3. command to stop windchill###
 echo "Start : Stopping windchill"
 ./windchill stop
+echo pwd
 echo "End : Windchill stopped"
 
 ### 4. Navigate to the build folder ###
 echo "Start : Navigating to the build folder"
 cd ../SourceCode/Build
+echo pwd
 echo "End : Navigating to the build folder"
 
 
 ### 5. Execute the ant command ###
 echo "Start : Executing ant command"
+echo pwd
 ant
 echo "End : Executing ant command done"
 
