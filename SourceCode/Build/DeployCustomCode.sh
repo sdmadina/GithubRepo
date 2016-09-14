06e6ffefc060f45aee93370a6ae1d588c9e05676
Devendra Bobde <devendra.bobde@itcinfotech.com>
2016-09-14 15:35:15 +0530

    Modified script for testing ant command

--- SourceCode/Build/DeployCustomCode.sh
+++ SourceCode/Build/DeployCustomCode.sh
@@ -27,6 +27,7 @@ echo "End : Navigating to the build folder"
 ### 5. Execute the ant command ###
 echo "Start : Executing ant command"
 WT_HOME=/opt/ptc/Windchill_11.0/Windchill/
+JAVA_HOME=/opt/ptc/Windchill_11.0/Java/jre/bin/java
 /opt/ptc/Windchill_11.0/Windchill/ant/bin/ant
 echo "End : Executing ant command done"
 
